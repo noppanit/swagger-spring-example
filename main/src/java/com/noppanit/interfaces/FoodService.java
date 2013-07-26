07b60dd29cdfbe73176e0f6f6ee7a9cab25aef8d
Noppanit Charassinvichai <noppanit.c@gmail.com>
2013-07-26 13:37:00 +0100

    First test is ok now

---
+++ main/src/java/com/noppanit/interfaces/FoodService.java
@@ -0,0 +1,10 @@
+package com.noppanit.interfaces;
+/**
+ * Created with IntelliJ IDEA.
+ * User: ncharass
+ * Date: 26/07/2013
+ * Time: 13:23
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FoodService {
+}
---
+++ main/src/java/com/noppanit/model/Food.java
@@ -0,0 +1,29 @@
+package com.noppanit.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ncharass
+ * Date: 26/07/2013
+ * Time: 13:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Food {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
